65119812ba86cbff639482ff9cd211714af787ca
John Gilik <john@jgilik.com>
2015-08-20 10:49:22 -0400

    Initial commit of client-cert variant of wget.
    (It will also support HTTPS proxying with client certs in
    short order, as that's ultimately its main use case.)

---
+++ sget/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	stdout = flag.Bool("O-", false, "Whether to save to stdout")
+	_ = flag.Bool("retry-connrefused", false, "non-operation; exists for wget compatibility")
+	keyPath = flag.String("key", "/opt/ssl/client.key", "Private key for client certificate.")
+	certPath = flag.String("cert", "/opt/ssl/client.crt", "Client certificate file.")
+	caPath = flag.String("ca", "/opt/ssl/ca.crt", "Certificate authority certificate file.")
+)
+
+func main() {
+	flag.Parse()
+
+	cert, err := tls.LoadX509KeyPair(*certPath, *keyPath)
+	if err != nil {
+		log.Fatalf("Failed to read client certificate %q and key %q: %v",
+			*certPath, *keyPath, err)
+	}
+
+	ca, err := ioutil.ReadFile(*caPath)
+	if err != nil {
+		log.Fatalf("Failed to read CA certificate %q: %v", *caPath, err)
+	}
+	caPool := x509.NewCertPool()
+	caPool.AppendCertsFromPEM(ca)
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs: caPool,
+	}
+	transport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	urls := flag.Args()
+	if len(urls) != 1 {
+		log.Fatalf("Incorrect number of URLs: got %d; want 1", len(urls))
+	}
+	url := urls[0]
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatalf("GET failed for URL %q: %v", url, err)
+	}
+	if *stdout {
+		buf := make([]byte, 512, 512)
+		for {
+			n, err := resp.Body.Read(buf)
+			os.Stdout.Write(buf[0:n])
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				log.Fatalf("Error reading response body: %v", err)
+			}
+		}
+	} else {
+		log.Fatalf("Currently only stdout output is supported")
+	}
+}
